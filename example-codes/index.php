48793956ace63cd7cceca1dcdf50063e58146a33
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-03-31 18:24:59 +0300

    Enhance output and add curl initializations
    Added additional output statements and curl initializations.

--- example-codes/index.php
+++ example-codes/index.php
@@ -14,3 +14,18 @@ eval($code);
 
 
 $output = shell_exec($command);
+
+
+echo $output;
+
+echo $output;
+
+echo $name;
+
+$name = htmlspecialchars($name);
+echo $name;
+
+$code    = $_GET['code'];
+curl_init($name);
+curl_init($code);
+curl_init($asd);
