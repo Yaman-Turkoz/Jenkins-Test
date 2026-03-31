cbcc15ad70f065cbecbeb2888661bd90a78e9d75
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-03-31 10:42:59 +0300

    Document security issues in index.php
    Added comments highlighting security vulnerabilities in the code.

--- example-codes/index.php
+++ example-codes/index.php
@@ -4,4 +4,19 @@ $name    = $_GET['name'];
 $command = $_GET['cmd'];
 $code    = $_GET['code'];
 
+// XSS: kullanıcı girdisi doğrudan çıktılanıyor
+echo $name;
 
+// Debug: hassas veri sızdırabilir
+var_dump($name);
+
+// RCE: kullanıcı girdisi doğrudan eval'e gönderiliyor
+eval($code);
+
+// Command Injection: kullanıcı girdisi shell'e gönderiliyor
+$output = shell_exec($command);
+echo $output;
+
+echo $output;
+
+echo $name;
