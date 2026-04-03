db6ddc0a991a2fe202d83737753b30ad7647cd9f
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-04-03 15:47:29 +0300

    Update ssrf.php

--- example-codes/ssrf.php
+++ example-codes/ssrf.php
@@ -1,15 +1,28 @@
 <?php
-    $url = $_GET['url'];
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($curl, CURLOPT_TIMEOUT, 5);
-    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
-    $data = curl_exec ($curl);
-    if(curl_error($curl)){
-        echo curl_error($curl);
-    }else{
-        echo "<pre>" . $data . "</pre>";
-    }
-    curl_close ($curl);
+
+if( isset( $_GET[ 'Change' ] ) ) {
+
+	$pass_new  = $_GET[ 'password_new' ];
+	$pass_conf = $_GET[ 'password_conf' ];
+
+
+	if( $pass_new == $pass_conf ) {
+
+		$pass_new = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $pass_new ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
+		$pass_new = md5( $pass_new );
+
+		$current_user = dvwaCurrentUser();
+		$insert = "UPDATE `users` SET password = '$pass_new' WHERE user = '" . $current_user . "';";
+		$result = mysqli_query($GLOBALS["___mysqli_ston"],  $insert ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+
+		$html .= "<pre>Password Changed.</pre>";
+	}
+	else {
+
+		$html .= "<pre>Passwords did not match.</pre>";
+	}
+
+	((is_null($___mysqli_res = mysqli_close($GLOBALS["___mysqli_ston"]))) ? false : $___mysqli_res);
+}
+
 ?>
