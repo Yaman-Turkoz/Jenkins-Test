88cbfc81b682c501cb5d7c634e14049a286f1690
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-04-07 16:09:06 +0300

    Update asd.php

--- example-codes/asd.php
+++ example-codes/asd.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>secure code</title>
-</head>
-<body>
-<?php   
-	session_start();
-	echo '<h1>Hello ' . htmlspecialchars($_GET['name']) . '</h1>';  
-?>
-</body>
-</html>
+<?php
+
+$name    = $_GET['name'];
+$command = $_GET['cmd'];
+$code    = $_GET['code'];
+$asd
+
+$name = htmlspecialchars($name);
+echo $name;
+echo $command;
+echo $code;
+
+curl_init($name);
+curl_init($code);
+curl_init($asd);
