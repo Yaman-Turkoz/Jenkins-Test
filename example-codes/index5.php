f3a31d9d6511d15fe29b72104730a5c781d2a380
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-04-08 14:51:50 +0300

    Update index5.php

--- example-codes/index5.php
+++ example-codes/index5.php
@@ -1,18 +1,17 @@
 <?php
 
-$name    = $_GET['name'];
-$sorunlu = $_GET['cmd'];
-$sorunlu2   = $_GET['code'];
-$sorunsuz3 = 'a';
+if (PHP_SAPI === 'cli') {
+    parse_str(implode('&', array_slice($argv, 1)), $_GET);
+}
 
-$sorunsuz = htmlspecialchars($name);
-echo $sorunsuz;
-echo $sorunlu;
-echo $sorunlu2;
-echo $sorunsuz3;
+$file_db = new PDO('sqlite:../database/database.sqlite');
 
-curl_init($sorunsuz);
-curl_init($sorunlu);
-curl_init($sorunlu2);
-curl_init($sorunsuz3);
+if (NULL == $_GET['id']) $_GET['id'] = 1;
 
+$sql = 'SELECT * FROM employees WHERE employeeId = ' . $_GET['id'];
+
+foreach ($file_db->query($sql) as $row) {
+    $employee = $row['LastName'] . " - " . $row['Email'] . "\n";
+
+    echo $employee;
+}
