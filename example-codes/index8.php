7229e4f918830c5b0167b695cb387480298fdd0f
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-04-08 10:50:52 +0300

    Update index8.php

--- example-codes/index8.php
+++ example-codes/index8.php
@@ -1,5 +1,15 @@
 <?php
 
-$a = $_GET["a"];
+$name    = $_GET['name'];
+$command = $_GET['cmd'];
+$code    = $_GET['code'];
+$asd
 
-echo $a;
+$name = htmlspecialchars($name);
+echo $name;
+echo $command;
+echo $code;
+
+curl_init($name);
+curl_init($code);
+curl_init($asd);
