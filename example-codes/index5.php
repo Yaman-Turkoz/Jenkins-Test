96118ea65256f42cb6ca54c57b7f3ed758bd0058
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-04-13 15:29:08 +0300

    Update index5.php

--- example-codes/index5.php
+++ example-codes/index5.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>secure code</title>
-</head>
-<body>
-<?php   
-	session_start();
-	echo '<h1>Hello ' . $_GET['name'] . '</h1>';  
+<?php
+
+header ("X-XSS-Protection: 0");
+
+// Is there any input?
+if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
+	// Get input
+	$name = preg_replace( '/<(.*)s(.*)c(.*)r(.*)i(.*)p(.*)t/i', '', $_GET[ 'name' ] );
+
+	// Feedback for end user
+	$html .= "<pre>Hello {$name}</pre>";
+}
+
 ?>
-</body>
-</html>
