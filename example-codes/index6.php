2ff61877a9526dda82bd10465d712d5bf9c26e12
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-04-14 15:40:13 +0300

    Refactor input handling and add X-XSS-Protection header
    Removed old input sanitization and database update code while adding X-XSS-Protection header and feedback for the user.

--- example-codes/index6.php
+++ example-codes/index6.php
@@ -1,22 +1,14 @@
 <?php
 
-if( isset( $_POST[ 'btnSign' ] ) ) {
+header ("X-XSS-Protection: 0");
+
+// Is there any input?
+if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
 	// Get input
-	$message = trim( $_POST[ 'mtxMessage' ] );
-	$name    = trim( $_POST[ 'txtName' ] );
+	$name = preg_replace( '/<(.*)s(.*)c(.*)r(.*)i(.*)p(.*)t/i', '', $_GET[ 'name' ] );
 
-	// Sanitize message input
-	$message = stripslashes( $message );
-	$message = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $message ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
-
-	// Sanitize name input
-	$name = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $name ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
-
-	// Update database
-	$query  = "INSERT INTO guestbook ( comment, name ) VALUES ( '$message', '$name' );";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
-
-	//mysql_close();
+	// Feedback for end user
+	$html .= "<pre>Hello {$name}</pre>";
 }
 
 ?>
