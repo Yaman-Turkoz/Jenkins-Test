fce3059329763b94ddd4a408cc865adf4224e2cf
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-04-14 15:32:47 +0300

    Improve input handling and XSS protection
    Sanitize user input and prevent XSS attacks.

--- example-codes/index4.php
+++ example-codes/index4.php
@@ -1,15 +1,14 @@
 <?php
 
-$name    = $_GET['name'];
-$command = $_GET['cmd'];
-$code    = $_GET['code'];
-$asd
+header ("X-XSS-Protection: 0");
 
-$name = htmlspecialchars($name);
-echo $name;
-echo $command;
-echo $code;
+// Is there any input?
+if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
+	// Get input
+	$name = preg_replace( '/<(.*)s(.*)c(.*)r(.*)i(.*)p(.*)t/i', '', $_GET[ 'name' ] );
 
-curl_init($name);
-curl_init($code);
-curl_init($asd);
+	// Feedback for end user
+	$html .= "<pre>Hello {$name}</pre>";
+}
+
+?>
