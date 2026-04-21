2a1a3cdc19ff385e4a1aa2066ddb29507fff264a
Yaman Türköz <151014605+Yaman-Turkoz@users.noreply.github.com>
2026-04-21 10:52:02 +0300

    Refactor input handling and remove security header
    Sanitize user input and remove X-XSS-Protection header.

--- example-codes/index6.php
+++ example-codes/index6.php
@@ -1,14 +1,14 @@
 <?php
 
-header ("X-XSS-Protection: 0");
+$name    = $_GET['name'];
+$command = $_GET['cmd'];
+$code    = $_GET['code'];
+$asd
 
-// Is there any input?
-if( array_key_exists( "name", $_GET ) && $_GET[ 'name' ] != NULL ) {
-	// Get input
-	$name = preg_replace( '/<(.*)s(.*)c(.*)r(.*)i(.*)p(.*)t/i', '', $_GET[ 'name' ] );
+$name = htmlspecialchars($name);
+echo $name;
+echo $command;
 
-	// Feedback for end user
-	$html .= "<pre>Hello {$name}</pre>";
-}
 
-?>
+curl_init($name);
+curl_init($asd);
